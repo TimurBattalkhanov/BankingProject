e450d4ad82c7e672c6e2057b553ae72f238c4d2d
Timur Battalkhanov <32982157+TimurBattalkhanov@users.noreply.github.com>
2018-12-04 20:51:42 +0600

    Update Checking.java

--- src/Checking.java
+++ src/Checking.java
@@ -1,13 +1,14 @@
 import java.sql.SQLOutput;
 
 public class Checking extends Account {
-
+    //Specific parameters to Checking account type
     private long debitCardNumber;
     private int debitCardPIN;
 
     public Checking(String name, String sSN, double initDeposit) {
         super(name, sSN, initDeposit);
-        accountNumber = "2" + accountNumber;
+        //first digit in account number shows type of account (2 is Checking)
+        accountNumber = "2" + accountNumber; 
         setDebitCard();
     }
 
@@ -16,12 +17,12 @@ public class Checking extends Account {
         rate = getBaseRate() * 0.15;
 
     }
-
+//Randomly set 12 digit number to new account and 4 digit number to PIN code
     private void setDebitCard() {
         debitCardNumber = (long) (Math.random() * Math.pow(10, 12));
         debitCardPIN = (int) (Math.random() * 10000);
     }
-
+//Method shows specific info to Checking accounts + common Account info
     @Override
     public void showInfo() {
         super.showInfo();
