03d41753b1a296b5bff19ff49b37b75cc6871bc4
Timur Battalkhanov <32982157+TimurBattalkhanov@users.noreply.github.com>
2018-12-04 20:38:21 +0600

    Update BankAccountApp.java

--- src/BankAccountApp.java
+++ src/BankAccountApp.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class BankAccountApp {
     public static void main(String[] args) {
         List<Account> account = new LinkedList<>();
-
+//Reading data from utilities.CSV.read method
         String file = "D:\\NewBankAccounts.csv";
         List<String[]> newAccounts = utilities.CSV.read(file);
         for (String[] accounts : newAccounts) {
